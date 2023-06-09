69b38ee8310d4a91ee1c2e806aabb9662cc18b3d
Gerard Escolies <gerard.esma@icloud.com>
2023-06-09 13:26:46 +0000

    update git ignore

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
+import i18n from "astro-i18n"
 import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [i18n(),preact()]
 });
