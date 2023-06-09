d00abd28efec51eb09c56023c2ea42b84135b177
Gerard Escolies <gerard.esma@icloud.com>
2023-06-09 15:33:57 +0000

    add translations to the portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
+import astroI18next from "astro-i18next";
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [astroI18next(), preact()]
 });
