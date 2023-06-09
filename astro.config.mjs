d3eb48afa3a15317bd72ae6dff63e047738d679d
Gerard Escolies <gerard.esma@icloud.com>
2023-06-09 15:07:15 +0000

    delete i18n plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
-import i18n from "astro-i18n"
 import { defineConfig } from 'astro/config';
-
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [i18n(),preact()]
+  integrations: [preact()]
 });
