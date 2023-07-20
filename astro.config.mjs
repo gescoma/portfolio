ae05f5cebb9c827f3da91ebb004417870f89e15e
Gerard Escolies <gerard.esma@icloud.com>
2023-07-20 12:35:00 +0000

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import astroI18next from "astro-i18next";
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
+import vercel from '@astrojs/vercel/edge';
 
 // https://astro.build/config
 export default defineConfig({
